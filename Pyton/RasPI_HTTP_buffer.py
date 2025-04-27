8ca7e0c209290bff394ff6f21edd9c915bfac8f9
P3T3R2002 <banyai.p0227@gmail.com>
2025-04-27 13:36:37 +0200

    generate rails

--- Pyton/RasPI_HTTP_buffer.py
+++ Pyton/RasPI_HTTP_buffer.py
@@ -6,21 +6,20 @@ import csv
 import os
 
 
-STATIC_IP = "192.168.0.200"
+STATIC_IP = "192.168.244.77"
 PORT = "8080"
 FILE_PATH = "/mnt/d/Desktop/Peti/egyetem/Szakdolgozat/"
 
 URL = 'http://' + STATIC_IP + ":" + PORT
-
-def index():
-    return "Hello there!"
 
 def get_data():
     contents = urllib.request.urlopen(URL).read()
     return contents
 
 def get_date():
-    contents = urllib.request.urlopen(URL + "/date").read()
+    print("date_1")
+    contents = urllib.request.urlopen(URL + "/date", timeout=5).read()
+    print("date_2")
     return contents
 
 def log_error(LOG_txt, e):
@@ -36,41 +35,44 @@ def main():
     log_error(LOG_txt, "Logging start...")
 
 
-    try:
-        reference_date = time.time()-json.loads(get_date())["date"]/1000
-    except Exception as e:
-        log_error(LOG_txt, e)
-
     while True:
         try:
-            date = datetime.datetime.now()
+            reference_date = time.time()-json.loads(get_date())["date"]/1000
 
-            name_speed = f"WINDSDATA_{date.year}_{date.month}_{date.day}.csv"
-            day = date.day
-            current_day = day
+            while True:
+                try:
+                    date = datetime.datetime.now()
 
-            SPEED_PATH = os.path.join(ROOT_DIR, FILE_PATH+name_speed)
+                    name_speed = f"WINDSDATA_{date.year}_{date.month}_{date.day}.csv"
+                    day = date.day
+                    current_day = day
 
-            DATA_csv = open(SPEED_PATH, "w", newline="")
-            DATA_writer = csv.writer(DATA_csv)
-            DATA_writer.writerow(["SPEED", "ANGLE", "DATE"])
+                    SPEED_PATH = os.path.join(ROOT_DIR, FILE_PATH+name_speed)
+
+                    DATA_csv = open(SPEED_PATH, "w", newline="")
+                    DATA_writer = csv.writer(DATA_csv)
+                    DATA_writer.writerow(["SPEED", "ANGLE", "DATE"])
+                except Exception as e:
+                    log_error(LOG_txt, e)
+
+                while day == current_day:
+                    try:
+                        current_time = datetime.datetime.now()
+                        data = json.loads(get_data())
+                        data["date"] = reference_date + data["date"]/1000
+                        DATA_writer.writerow(data.values())
+
+                    except Exception as e:
+                        log_error(LOG_txt, e)
+
+                    current_day = current_time.day
+
+                print("close")
+                DATA_csv.close()
+
         except Exception as e:
+            print(e)
             log_error(LOG_txt, e)
-
-        while day == current_day:
-            try:
-                current_time = datetime.datetime.now()
-                data = json.loads(get_data())
-                data["date"] = reference_date + data["date"]/1000
-                DATA_writer.writerow(data.values())
-
-            except Exception as e:
-                log_error(LOG_txt, e)
-
-            current_day = current_time.day
-
-        print("close")
-        DATA_csv.close()
 
 if __name__ == "__main__":
     main()
