330cbcd2b4d1d7f7ffc802f1197a9248ce94a800
P3T3R2002 <banyai.p0227@gmail.com>
2025-05-01 21:22:03 +0200

    show/download

--- Rails/szakdoga/app/controllers/home_controller.rb
+++ Rails/szakdoga/app/controllers/home_controller.rb
@@ -2,12 +2,16 @@ require 'net/http'
 require 'date'
 require 'json'
 require 'uri'
+require 'csv'
 
 class HomeController < ApplicationController
   protect_from_forgery with: :null_session  # Prevent CSRF errors for API-style requests
   
-  IP = "192.168.0.201"
   PORT = "5000"
+  IP = "192.168.0.201"
+  DATA_FILE_PATH = "/mnt/d/Desktop/Peti/egyetem/Szakdolgozat/WIND_DATA/"
+  MATLAB_PATH = "/mnt/c/Peti.C/MATLAB/R2023b/bin/matlab.exe"
+  SCRIPT_PATH = "D:/Desktop/Peti/egyetem/Szakdolgozat/scripts"
 
   def index
   end
@@ -27,32 +31,48 @@ class HomeController < ApplicationController
       params[:day_to].to_i
     ) rescue nil
 
+    action = params[:select_action]
     avg = params[:request_avg]
     
     date_data = {
       from_date: from_date,
       to_date: to_date,
+      action: action,
       avg: avg
-    }
+    }    
 
     # Save this date data as a JSON file (optional)
     File.open(Rails.root.join('public', 'date_data.json'), 'w') do |file|
       file.write(date_data.to_json)
     end
 
-    # Remote server URL and IP
-    # remote_server_ip = "http://#{IP}:#{PORT}" 
-    # uri = URI.join(remote_server_ip, "/#{action}")
+    case action
+      when "Download"
+        handle_download_response(send_download_request(date_data))
+      when "Show"
+        handle_show_response(send_show_request(date_data))
+    end
+  end
 
+  #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-#
+
+  def send_show_request(date_data)
 
     remote_server_ip = "http://0.0.0.0:#{PORT}"
-    uri = URI.join(remote_server_ip, "/") 
+    uri = URI.join(remote_server_ip, "/show") 
 
-    # Append the from_date and to_date as query parameters
-    uri.query = URI.encode_www_form(from_date: from_date.to_s, to_date: to_date.to_s, avg: avg.to_s)
-
+    # Append the from_date, to_date and action as query parameters
+    uri.query = URI.encode_www_form(from_date: date_data[:from_date].to_s, 
+                                    to_date: date_data[:to_date].to_s, 
+                                    avg: date_data[:avg].to_s
+                                    )
     # Send the GET request to the remote server
-    response = Net::HTTP.get_response(uri)
+    return Net::HTTP.get_response(uri)
+  end
+
+  #----------------------#
+
+  def handle_show_response(response)
 
     if response.is_a?(Net::HTTPSuccess)
       # Parse the response (assuming it's JSON)
@@ -88,5 +108,71 @@ class HomeController < ApplicationController
       flash[:notice] = "Failed to get data from remote server."
       redirect_to root_path
     end
+  end
+
+  #-------------------------------------------------------------#
+
+  def send_download_request(date_data)
+
+    remote_server_ip = "http://0.0.0.0:#{PORT}"
+    uri = URI.join(remote_server_ip, "/download") 
+
+    # Append the from_date, to_date and action as query parameters
+    uri.query = URI.encode_www_form(from_date: date_data[:from_date].to_s, 
+                                    to_date: date_data[:to_date].to_s
+                                    )
+    # Send the GET request to the remote server
+    return Net::HTTP.get_response(uri)
+  end
+
+  #----------------------#
+  
+  def handle_download_response(response)
+    if response.is_a?(Net::HTTPSuccess)
+        # Parse the response (assuming it's JSON)
+        remote_data = JSON.parse(response.body)
+
+      if !remote_data["data"].empty?
+        file_path = DATA_FILE_PATH + "WINDSPEED_#{remote_data["from_date"]}_#{remote_data["to_date"]}.csv"
+        
+        entries = []
+        remote_data["data"].each do |file|
+          entries += file["data"]
+        end
+
+        # Write to CSV
+        CSV.open(file_path, "w") do |csv|
+          csv << ["DATE", "SPEED", "ANGLE"]  # CSV header
+
+          entries.each do |entry|
+            csv << [entry["DATE"].to_f, entry["SPEED"].to_i, entry["ANGLE"].to_i]
+          end
+        end
+
+        flash[:notice] = "Data downloaded to #{file_path}"
+        if start_matlab = params[:start_matlab]
+          run_matlab_plot(Date.new(2025, 4, 28))
+        end
+        redirect_to root_path
+      else
+        flash[:notice] = "No data on remote server."
+        redirect_to root_path
+      end
+    else
+      flash[:notice] = "Failed to get data from remote server."
+      redirect_to root_path
+    end
+  end
+
+  #----------------------#
+
+  def run_matlab_plot(date)
+    year  = date.year
+    month = date.month
+    day   = date.day
+    
+    command = "addpath('#{SCRIPT_PATH}'); winds_plot(#{year}, #{month}, #{day}); exit;"
+    system("#{MATLAB_PATH} -r \"#{command}\"")
+
   end
 end
